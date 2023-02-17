d77d865623493326d6ea3e8dc216ec37457a5470
msobhi412 <msobhi412@gmail.com>
2023-02-18 00:13:03 +0200

    sdf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
-#include <stdio.h>
+#include <stido.h>
+
 /**
-* main - Entry
-* Return: Always 0
-*/
+ * main - Entry opint
+ * Return: Always 0
+ */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("/"Programming is like building a multilingual puzzle");
+	return (0);
 }
+
