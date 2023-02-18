92dc077a96e81143bb23c2ccdb138c686aff9009
msobhi412 <msobhi412@gmail.com>
2023-02-18 04:37:31 +0200

    sdfs

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	char i;
+
+	i = 'z';
+
+	while (i >= 'a')
+	{
+		putchar(i);
+		i--;
+	}
+	putchar('\n');
+	return (0);
+}
+
