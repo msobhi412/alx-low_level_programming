a0717979493dfc27fef59cbf924818023397e8fc
msobhi412 <msobhi412@gmail.com>
2023-02-18 04:07:41 +0200

    sdfsd

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while (c <= 'z')
+	{
+		if ((c != 'q' && c != 'e') && c <= 'z')
+			putchar();
+		
+		c++;
+	}
+	
+	purchar('\n');
+	return (0);
+}
+
