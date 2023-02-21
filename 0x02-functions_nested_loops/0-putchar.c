c24a84e13c8ee10c2e6757b37c4611ecebf2a32b
msobhi412 <msobhi412@gmail.com>
2023-02-21 16:06:19 +0200

    sdf

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/* more headers goes there */
+/**
+* main - Entry point
+*
+* Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	printf("_putchar");
+}
+
